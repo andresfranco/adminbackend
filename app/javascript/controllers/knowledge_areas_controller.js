9fdeb322304c3f497f4922fac2a941f7333eb082
Andres Franco <koloinotzente@gmail.com>
2022-08-20 12:30:37 -0500

    Refactoring methods in CustomAlerts

--- app/javascript/controllers/knowledge_areas_controller.js
+++ app/javascript/controllers/knowledge_areas_controller.js
@@ -7,11 +7,32 @@ export default class extends Controller {
 
   showDeleteAlert(event) {
     const customAlerts = new CustomAlerts()
+    const taskURL = event.target.getAttribute('href')
     var alertParams ={
       title:'Are you sure you want to delete this record?',
       showCancelButton : true,
       confirmButtonText :'Yes',
-      reloadWindow:true
+      reloadWindow:true,
+      taskURL:taskURL,
+      errorMessage:'Error:Deletion of this Knowlege base failed',
+      errorTitle:'Error',
+      errorAlertType:'error',
+      errorAlertTimer:false,
+      errorAlertTime:0,
+      errorAlertRefreshWindow:false,
+      errorAlertRefreshWindowTime:false,
+      successStatusCode:200,
+      successStatusText:'',
+      successTitle:'Success',
+      successMessage:'The Knowledge area has been deleted',
+      successAlertType:'success',
+      sucessAlertTimer:false,
+      successAlertTime:0,
+      successAlertRefreshWindow:true,
+      successAlertRefreshWindowTime:2000,
+      customDeleteUrl:'knowledge_areas/delete_fetch/',
+      customUrlMainPath:'knowledge_areas/'
+
     }
     customAlerts.getDeleteAlert(event,alertParams,'fetch')
   
--- app/javascript/utils/customAlerts.js
+++ app/javascript/utils/customAlerts.js
@@ -2,105 +2,120 @@ import Swal from "sweetalert2"
 import Rails from "@rails/ujs";
 import {getAjaxErrorMessage} from "../utils/errorHandler"
 
-
-export function getAlertMessage(status,statusText,alertTitle,alertType,errorMessage,hasTimer,time,refreshWindow,refresWindowTime){
-  if(alertType ==='error'){errorMessage = getAjaxErrorMessage(status,statusText)}
-  hasTimer ?setTimeout(function(){Swal.fire(alertTitle,errorMessage,alertType)}, time):Swal.fire(alertTitle,errorMessage,alertType)
-  if(refreshWindow){refreshScreenTimer(refresWindowTime,refreshWindow) }
-}
-  
-
-export function refreshScreenTimer(time,refreshWindow){
-  setTimeout(function(){
-    if(refreshWindow){window.location.reload()} 
-  },time)
-}
 export class CustomAlerts {
-     constructor(){
 
-     }
-    setDeleteAlertValues(event){
-        const deleteAlertValues ={ 
-            methodType:"Delete",
-            taskURL:event.target.getAttribute('href'),
-            contentType:'application/json',
-            errorMessage:'Request Error:'
-        } 
+  getAlertMessage(status,statusText,alertTitle,alertType,errorMessage,hasTimer,time,refreshWindow,refresWindowTime){
+    if(alertType ==='error') errorMessage = errorMessage+': '+getAjaxErrorMessage(status,statusText)
+    hasTimer ?setTimeout(function(){Swal.fire(alertTitle,errorMessage,alertType)}, time):Swal.fire(alertTitle,errorMessage,alertType)
+    if(refreshWindow) this.refreshScreenTimer(refresWindowTime,refreshWindow)
+  }
+
+  refreshScreenTimer(time,refreshWindow){
+    setTimeout(function(){
+      if(refreshWindow) window.location.reload()
+    },time)
+  }
+
+  getCustomDeleteUrl(taskUrl,pathToReplace,replacementValue){
+      return taskUrl.replace(pathToReplace,replacementValue)
+  }
+
+  getCRFToken(){
+    var crfToken = document.getElementsByName("csrf-token")[0].content
+     return crfToken != null?crfToken:""
+  }
+    
+    setDeleteAlertValues(alertType,alertParams){
+        var deleteAlertValues={}
+        if(alertType ==='delete'){
+          deleteAlertValues=
+          { 
+            methodType:'Delete',
+            taskURL:alertParams.taskURL,
+            contentType:'application/json'
+          } 
+        }
         return deleteAlertValues
         
     }
      
      getDeleteAlert(event,alertParams,alertType){
-      var customAlert = new CustomAlerts()
       switch (alertType) {
         case 'ajax':
-          customAlert.confirmDeleteAlertRailsAjax(event,alertParams)
+          this.confirmDeleteAlertRailsAjax(event,alertParams)
         case 'fetch':
-          customAlert.confirmDeleteAlertFetch(event,alertParams)
+          this.confirmDeleteAlertFetch(event,alertParams)
           break;
         default:
-          customAlert.confirmDeleteAlertFetch(event,alertParams)
+          this.confirmDeleteAlertFetch(event,alertParams)
       }
      }     
 
     confirmDeleteAlertRailsAjax(event,alertParams){
       event.preventDefault()
-      const deleteAlertValues = this.setDeleteAlertValues(event)
+      const _this =this
+      const deleteAlertValues = this.setDeleteAlertValues('delete',alertParams)
       Swal.fire({
       title:alertParams.title,
       showCancelButton:alertParams.showCancelButton ,
-      confirmButtonText: alertParams.confirmButtonText 
-    }).then((result) => {
+      confirmButtonText: alertParams.confirmButtonText })
+      .then((result) => {
       if (result.isConfirmed) {
-        Rails.ajax({
-          type:deleteAlertValues.methodType,
-          url:deleteAlertValues.taskURL,
-          success:function(){
-            getAlertMessage(200,'',"Success",'success','The Knowledge area has been deleted',false,0,true,2000)
-          },
-          error:function(response,statusText,xhr){
-            getAlertMessage(xhr.status,xhr.statusText,"Error",'error','Error',false,0,false,0)
-          }
+          Rails.ajax({
+           type:deleteAlertValues.methodType,
+           url:deleteAlertValues.taskURL,
+           success:()=>{
+             _this.getAlertMessage(alertParams.successStatusCode,alertParams.successStatusText
+                  ,alertParams.successTitle,alertParams.successAlertType
+                  ,alertParams.successMessage,alertParams.sucessAlertTimer,alertParams.successAlertTime
+                  ,alertParams.successAlertRefreshWindow,alertParams.successAlertRefreshWindowTime)
+           },
+           error:(response,statusText,xhr)=>{ 
+            _this.getAlertMessage(xhr.status,xhr.statusText,alertParams.errorTitle
+                 ,alertParams.errorAlertType,alertParams.errorMessage,alertParams.errorAlertTimer
+                 ,alertParams.errorAlertTime,alertParams.errorAlertRefreshWindow,alertParams.errorAlertRefreshWindowTime)
+           }
         })
-        //}
+        
       }
     })
     }
      
-    ///"skip_before_action :verify_authenticity_token" is assigned to the method in the Rails controller only if need to skip the use of CSRF token
-    // call in view:  <%= link_to "delete fetch", knowledge_area, data: { turbo_method: :delete, action:'click->knowledge-areas#showFetchAlert' } %>
-    //Call in javascript method: customAlerts.confirmDeleteAlertFetch(event,alertParams)
     confirmDeleteAlertFetch(event,alertParams){
-        
         event.preventDefault()
-        const csrfToken = document.getElementsByName("csrf-token")[0].content;
-        const deleteAlertValues = this.setDeleteAlertValues(event)
-        const taskURL =deleteAlertValues.taskURL.replace("knowledge_areas/","knowledge_areas/delete_fetch/")
-        this.setDeleteAlertValues(event)
+        const csrfToken = this.getCRFToken()
+        const deleteAlertValues = this.setDeleteAlertValues('delete',alertParams)
+        const taskURL =this.getCustomDeleteUrl(deleteAlertValues.taskURL,alertParams.customUrlMainPath,alertParams.customDeleteUrl)
         Swal.fire({
-        title:alertParams.title,
-        showCancelButton:alertParams.showCancelButton ,
-        confirmButtonText: alertParams.confirmButtonText 
-      }).then((result) => {
-        if (result.isConfirmed) {
-            return fetch(taskURL ,{
-                method:deleteAlertValues.methodType,
-                headers: {
+          title:alertParams.title,
+          showCancelButton:alertParams.showCancelButton ,
+          confirmButtonText: alertParams.confirmButtonText 
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+              return fetch(taskURL ,{
+                  method:deleteAlertValues.methodType,
+                  headers: {
                   'X-CSRF-Token': csrfToken,
                   'Content-Type': deleteAlertValues.contentType
                   }
-            })
-            .then(response => {
-                  response.ok ? getAlertMessage(response.status,'',"Success",'success','The Knowledge area has been deleted',false,0,true,2000)
-                  :getAlertMessage(response.status,response.statusText,"Error",'error','Error',false,0,false,0)
-            })
-            .catch(error => {
-              var errorMessage = deleteAlertValues.errorMessage+error.errorMessage
-              getAlertMessage(response.status,response.statusText,"Error",'error',errorMessage,false,0,false,0)
-            })
+              })
+              .then(response => {
+                  response.ok ? this.getAlertMessage(alertParams.successStatusCode,alertParams.successStatusText
+                    ,alertParams.successTitle,alertParams.successAlertType
+                    ,alertParams.successMessage,alertParams.sucessAlertTimer,alertParams.successAlertTime
+                    ,alertParams.successAlertRefreshWindow,alertParams.successAlertRefreshWindowTime)
+                  :this.getAlertMessage(response.status,response.statusText,alertParams.errorTitle
+                    ,alertParams.errorAlertType,alertParams.errorMessage,alertParams.errorAlertTimer
+                    ,alertParams.errorAlertTime,alertParams.errorAlertRefreshWindow,alertParams.errorAlertRefreshWindowTime)
+              })
+              .catch(error => {
+                this.getAlertMessage('',error,alertParams.errorTitle
+                ,alertParams.errorAlertType,alertParams.errorMessage,alertParams.errorAlertTimer
+                ,alertParams.errorAlertTime,alertParams.errorAlertRefreshWindow,alertParams.errorAlertRefreshWindowTime)
+              })
            
-        }
-      })
-      }
-
+          }
+        })
+     }
 }
